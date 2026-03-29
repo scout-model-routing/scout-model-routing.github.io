6584cb2de24ea50f8a4702070f466dd97720d9a8
Anakraj <akashmail1010@gmail.com>
2026-03-28 21:05:04 -0400

    Website

---
+++ static/images/reconstructions/scoutes_route.py
@@ -0,0 +1,12 @@
+routes = {
+    1: 'trellis',
+    2: 'instantmesh',
+    3: 'trellis',
+    4: 'hunyuan',
+    5: 'instantmesh',
+    6: 'instantmesh',
+    7: 'triposr',
+    8: 'triposr',
+    9: 'hunyuan',
+    10: 'hunyuan',
+}
